5da4ac2094444e429c59cab785f947ba57ea860f
Francis0804 <sicnarfaying.307@gmail.com>
2025-08-30 02:38:26 +0800

    modify allowed host

--- hangarinsite/hangarinsite/settings.py
+++ hangarinsite/hangarinsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c@38+t(!!abhweef&qq^=y84_+awtixn&f6-lcn^y0bbtzr+!^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['user08.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 
